f7efe774a2f5be12e476dbe81a105a1dc0b9f285
dapeli <terryinzaghi@163.com>
2019-09-03 16:33:03 +0800

    Update py2.install.sh

--- py2.install.sh
+++ py2.install.sh
@@ -1,5 +1,8 @@
 cd /mnt/sdf/PY2/ENV
 wget https://www.python.org/ftp/python/2.7.16/Python-2.7.16.tgz
 tar xzf Python-2.7.16.tgz
-mv Python-2.7.16 PY2.7.16
+mv Python-2.7.16 PY2.7.16-SRC
+mkdir PY2.7.16-INSTALLED
+cd PY2.7.16
+
 
