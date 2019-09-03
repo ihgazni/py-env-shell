dd9f9cd68b75390cb473db3cf62ed49cc9da55a1
dapeli <terryinzaghi@163.com>
2019-09-03 09:09:39 +0800

    Delete odide.install.sh

--- odide.install.sh
+++
@@ -1,6 +0,0 @@
-wget https://github.com/iodide-project/pyodide/releases/download/0.14.0/pyodide-build-0.14.0.tar.bz2
-mkdir ODIDE
-mv pyodide-build-0.14.0.tar.bz2 ODIDE
-cd ODIDE/
-tar -xvf pyodide-build-0.14.0.tar.bz2
-rm pyodide-build-0.14.0.tar.bz2
